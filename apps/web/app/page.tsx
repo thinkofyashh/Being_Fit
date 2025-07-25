bd5081f414120760cf957c28af153dff612382b8
yashrawat <yashrawat682@gmail.com>
2025-07-25 18:15:01 +0530

    initialized the taiwind css

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,26 +1,9 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className="bg-black text-white p-3 rounded-md text-lg font-semibold text-center w-fit mx-auto">
      hi there 
     </div>
   );
